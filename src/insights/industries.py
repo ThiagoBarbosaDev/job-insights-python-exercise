46e7ecc6b3c974c3a68e0ee6428c8f3ed7679ea6
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2023-02-18 22:16:22 -0300

    feat(industries): implement filter_by_industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -41,4 +41,7 @@ def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
     list
         List of jobs with provided industry
     """
-    raise NotImplementedError
+    try:
+        return list(job for job in jobs if job['industry'] == industry)
+    except ValueError:
+        raise NotImplementedError
